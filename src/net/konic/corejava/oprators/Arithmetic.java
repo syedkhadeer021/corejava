9843135d76a5b15d5370f381a27b38dd48c1fda3
syedkhadeer021 <syedkhadeer021@gmail.com>
2025-11-20 12:26:37 +0530

    operator

--- src/net/konic/corejava/oprators/Arithmetic.java
+++ src/net/konic/corejava/oprators/Arithmetic.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class Arithmetic {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		 System.out.print("Enter first number: ");
-	        int a = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
 
-	        System.out.print("Enter second number: ");
-	        int b = sc.nextInt();
+		System.out.print("Enter first number: ");
+		int a = sc.nextInt();
 
-	    
-	        System.out.println("\n--- Arithmetic Operators ---");
-	        System.out.println("Addition (a + b) = " + (a + b));
-	        System.out.println("Subtraction (a - b) = " + (a - b));
-	        System.out.println("Multiplication (a * b) = " + (a * b));
-	        System.out.println("Division (a / b) = " + (a / b));
-	        System.out.println("Modulus (a % b) = " + (a % b));
+		System.out.print("Enter second number: ");
+		int b = sc.nextInt();
 
+		System.out.println("Addition (a + b) = " + (a + b));
+		System.out.println("Subtraction (a - b) = " + (a - b));
+		System.out.println("Multiplication (a * b) = " + (a * b));
+		System.out.println("Division (a / b) = " + (a / b));
+		System.out.println("Modulus (a % b) = " + (a % b));
 
 	}
 
---
+++ src/net/konic/corejava/oprators/Relation.java
@@ -0,0 +1,25 @@
+package net.konic.corejava.oprators;
+
+import java.util.Scanner;
+
+public class Relation {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter first number :");
+		int a = sc.nextInt();
+
+		System.out.print("Enter second number :");
+		int b = sc.nextInt();
+
+		System.out.println("a == b : " + (a == b));
+		System.out.println("a != b : " + (a != b));
+		System.out.println("a > b  : " + (a > b));
+		System.out.println("a < b  : " + (a < b));
+		System.out.println("a >= b : " + (a >= b));
+		System.out.println("a <= b : " + (a <= b));
+
+	}
+
+}
