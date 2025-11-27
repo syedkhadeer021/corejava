6c17bcd13668f6e100785ad54759c20fcd81281c
syedkhadeer021 <syedkhadeer021@gmail.com>
2025-11-27 14:16:42 +0530

    Methods

---
+++ src/net/konic/corejava/Methodsss/NewMethodss.java
@@ -0,0 +1,20 @@
+package net.konic.corejava.Methodsss;
+
+public class NewMethodss {
+	public String add (String i,String j) {
+		String z= i+j;
+		System.out.println(z);
+		return z;
+	}
+		public static void main(String[] args) {
+			NewMethodss newmethodss = new NewMethodss();
+		newmethodss.add("khadeer","syed");
+			
+		
+	
+	
+}
+		
+	}
+
+
